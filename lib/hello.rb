610db3264560c83724d392295a7f1bb4e24ad8b1
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-19 20:40:08 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,28 @@
-def hello_t
-
+def hello_t(array)
+  # method will yield each element of the array to the block if a block is present 
+  if block_given? #returns true if the method is called with a block 
+    i = 0 
+  
+    while i < array.length #execute as long as i < length of array
+      # i = a given index number of our array in each iteration
+      # use this info to yield each successive value stored in the array 
+      # to the passed-in block:
+      yield array[i]
+      # [] method to grab the value of each successive index element as we proceed through our while loop
+      # yielding each to a block
+      i += 1 # increment the value of i
+    end
+  
+    array
+  else # mif block is not present, method will puts out a helpful phrase
+    puts "Hey! No block was given!"
+  end
 end
 
-# call your method here!
+#method can be called either with or without a block
 
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end

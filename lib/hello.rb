8ccb298d72a64abd8f205c81cea432a597a143fc
Ally Morson <ally.morson@gmail.com>
2019-10-02 19:32:52 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,11 +1,20 @@
-def hello_t (array)
-i = 0 
-
-while i < array.length 
-yield array[i]
-i = i + 1 
+def hello_t(array)
+  if block_given?
+    i = 0
+#i is equal to 0. The second time through the loop, i is 1, and so on. This will continue until i is equal to the index number of the last item in our array.
+    while i < array.length 
+      yield(array[i]) #use the bracket ([]) method to grab the value of each successive index element as we proceed through our while loop,
+      i = i + 1
 end
 
-# call your method here!
-["Tim", "Tom", "Jim"]
-hello_t("Tim", "Tom", "Jim")
+    array
+  else
+    puts "Hey! No block was given!"
+  end
+end
+
+["Tim", "Tom", "Jim"].each do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
